c0c485e57291f3702ac9ef028384f7fef6cac268
Deann Boric <insearchwhy@hotmail.com>
2024-07-02 11:56:58 +1000

    created user input error code

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # T1A3_Terminal_App
-# T1A3_Terminal_App
+
+- create input for user
+- import pdf capacity
+- create weekly report
+- If user goes over industry standards for cost to turnover ratio warn user
+
---
+++ main.py
@@ -0,0 +1,18 @@
+
+
+
+# For user input define errors
+def get_float_input(prompt):
+    while True:
+        try:
+            value = float(input(prompt))
+            if value < 0:
+                print("Please enter a positive number.")
+                continue
+            return value
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+
+
+
+
