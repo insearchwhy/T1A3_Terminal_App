5cf71daf499746e32ffa2bec6005c987bfc03eda
Deann Boric <insearchwhy@hotmail.com>
2024-07-02 17:35:42 +1000

    added modules

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 - create input for user
 - import pdf capacity
+- import error handling module
 - create weekly report
 - If user goes over industry standards for cost to turnover ratio warn user
 
--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+from fpdf import FPDF
+PDF = FPDF()
+import pyinputplus as pyip
+from datetime import datetime
+import os
 
 
 
@@ -5,17 +10,20 @@
 def get_float_input(prompt):
     while True:
         try:
-            value = float(input(prompt))
-            if value < 0:
-                print("Please enter a positive number.")
-                continue
+            value = pyip.inputFloat(prompt, min=0)
             return value
+        except pyip.RetryLimitException:
+            print("Too many retries. Exiting.")
+            exit(1)
+        except pyip.TimeoutException:
+            print("Timeout. Exiting.")
+            exit(1)
         except ValueError:
             print("Invalid input. Please enter a number.")
 
 # User welcome message and input fields
 def main():
-    print("Welcome to the Restraunant/Cafe Cost-Profit Analyzer")
+    print("Welcome to the Restaurant/Cafe Cost-Profit Analyzer")
     print("Please enter the following weekly costs:")
 
     turnover = get_float_input("Weekly turnover: $")
@@ -23,6 +31,16 @@ def main():
     cost_of_staffing = get_float_input("Cost of staffing: $")
     cost_of_rent = get_float_input("Cost of rent: $")
     cost_of_utilities = get_float_input("Cost of utilities: $")
+
+# User warnings if costs exceed industry predefined standards
+    if cost_of_goods > 0.3 * turnover:
+        print("Warning: Cost of goods sold exceeds 30% of turnover. Industry standard is 30% or below!.")
+    if cost_of_staffing > 0.3 * turnover:
+        print("Warning: Cost of staffing exceeds 30% of turnover. Industry standard is 30% or below.")
+    if cost_of_rent > 0.1 * turnover:
+        print("Warning: Cost of rent exceeds 10% of turnover. Industry standard is 10% or below!")
+    if cost_of_utilities > turnover:
+        print("Warning: Cost of utilities exceeds turnover. Industry standard is 10% or below!")
 
     total_costs = cost_of_goods + cost_of_staffing + cost_of_rent + cost_of_utilities
     gross_profit = turnover - total_costs
@@ -33,5 +51,4 @@ def main():
     print(f"Total Costs: ${total_costs:.2f}")
     print(f"Gross Profit: ${gross_profit:.2f}")
 
-if __name__ == "__main__":
-    main()
+
